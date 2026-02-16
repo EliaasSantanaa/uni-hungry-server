5dfd049822c9b0d87995c0045772a8489cc4112c
EliaasSantanaa <eliaassantana00@gmail.com>
2026-02-16 15:58:49 -0300

    feat: update main module import path and add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Application');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  app.useGlobalPipes(new ValidationPipe()); 
+
+  await app.listen(process.env.PORT ?? 5000);
+  logger.debug(`Servidor rodando http://192.168.1.29:${process.env.PORT ?? 5000}`);
 }
 bootstrap();
