8fa2180047cd365669bdd37ab019abf2b91c7cf1
EliaasSantanaa <eliaassantana00@gmail.com>
2026-02-17 14:34:01 -0300

    feat: add ResendModule and AuthModule to AppModule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from 'src/database/prisma.module';
+import { AuthModule } from 'src/module/auth/auth.module';
+import { ResendModule } from 'src/module/resend/resend.module';
 
 @Module({
   imports: [
@@ -10,6 +12,8 @@ import { PrismaModule } from 'src/database/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
+    ResendModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
